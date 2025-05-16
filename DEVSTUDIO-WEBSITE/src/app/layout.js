f5fe809cd57f607195e0e352ac0ba855e5c536aa
Raahim <127599591+Raahim2@users.noreply.github.com>
2025-05-16 14:58:38 +0530

    Update layout.js

--- DEVSTUDIO-WEBSITE/src/app/layout.js
+++ DEVSTUDIO-WEBSITE/src/app/layout.js
@@ -1,10 +1,32 @@
-import { ThemeProvider } from "../../components/themeprovider"
+import { ThemeProvider } from "@/components/theme-provider"
 import Footer from "../../components/footer"
+import Script from "next/script" 
+import Analytics from "./analytics";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
+      <head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-Y9TKTEM6QR"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-Y9TKTEM6QR', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </head>
+
       <body>
         <ThemeProvider
           attribute="class"
@@ -12,6 +34,7 @@ export default function RootLayout({ children }) {
           enableSystem
           disableTransitionOnChange
         >
+           <Analytics />
           <div className="flex min-h-screen flex-col">
             <main className="flex-grow">
               {children}
